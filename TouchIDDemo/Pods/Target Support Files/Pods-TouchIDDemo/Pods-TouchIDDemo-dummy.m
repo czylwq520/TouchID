e4658b5daff9240e383a1c15c704361e6258b2f8
笨孩子不笨 <1224246206@qq.com>
2019-03-17 12:24:33 +0800

    Revert "封装的TouchID功能"
    This reverts commit a932dfce84b335ba10ea838bfcc00948775002d5.

--- TouchIDDemo/Pods/Target Support Files/Pods-TouchIDDemo/Pods-TouchIDDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TouchIDDemo : NSObject
-@end
-@implementation PodsDummy_Pods_TouchIDDemo
-@end
--- TouchIDDemo/Pods/Target Support Files/Scale/Scale-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Scale : NSObject
-@end
-@implementation PodsDummy_Scale
-@end
